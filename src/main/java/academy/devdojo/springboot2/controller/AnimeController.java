ff42d2d9bd16150920127a36dd27263ef7331de8
williamsuane <william.suane@devdojo.academy>
2020-03-31 18:03:11 +0200

    SpringBoot 2 Essentials 07 - POST

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -40,5 +42,10 @@ public class AnimeController {
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime Not Found"));
 
         return ResponseEntity.ok(animeFound);
+    }
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime) {
+        return ResponseEntity.ok(animeRepository.save(anime));
     }
 }
--- src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
@@ -1,15 +1,29 @@
 package academy.devdojo.springboot2.repository;
 
 import academy.devdojo.springboot2.domain.Anime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class AnimeRepository {
 
-    public List<Anime> listAll() {
-        return List.of(new Anime(1, "Boku No Hero"),
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1, "Boku No Hero"),
             new Anime(2, "Berserk"),
-            new Anime(3, "Naruto"));
+            new Anime(3, "Naruto")));
+    }
+
+    public List<Anime> listAll() {
+        return animes;
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextInt(4, 100000));
+        animes.add(anime);
+        return anime;
     }
 }
