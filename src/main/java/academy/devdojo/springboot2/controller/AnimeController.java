d65ff8d2d098c0b9df61a48ac47b76e7ec7e7a4f
williamsuane <william.suane@devdojo.academy>
2020-03-31 18:32:11 +0200

    SpringBoot 2 Essentials 08 - DELETE

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,5 +48,11 @@ public class AnimeController {
     @PostMapping
     public ResponseEntity<Anime> save(@RequestBody Anime anime) {
         return ResponseEntity.ok(animeRepository.save(anime));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id) {
+        animeRepository.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
@@ -4,7 +4,9 @@ import academy.devdojo.springboot2.domain.Anime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
 
 @Repository
 public class AnimeRepository {
@@ -25,5 +27,12 @@ public class AnimeRepository {
         anime.setId(ThreadLocalRandom.current().nextInt(4, 100000));
         animes.add(anime);
         return anime;
+    }
+
+    public void delete(int id){
+        animes.remove(animes.stream()
+            .filter(anime -> anime.getId() == id)
+            .findFirst()
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime Not Found")));
     }
 }
