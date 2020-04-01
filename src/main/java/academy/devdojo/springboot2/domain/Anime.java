c131e1cbd59656920685b18f803e6bb41bff17fc
williamsuane <william.suane@devdojo.academy>
2020-04-01 21:45:00 +0200

    SpringBoot 2 Essentials 10 - Spring Data JPA with MySQL pt 01

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -1,12 +1,20 @@
 package academy.devdojo.springboot2.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Anime {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
 
