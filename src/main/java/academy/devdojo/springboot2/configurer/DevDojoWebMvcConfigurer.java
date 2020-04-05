304dac499846c1ce44770db4687685d16792dcde
williamsuane <william.suane@devdojo.academy>
2020-04-05 14:44:12 +0200

    SpringBoot 2 Essentials 20 - Paging and Sorting pt 02 - WebMvcConfigurer

---
+++ src/main/java/academy/devdojo/springboot2/configurer/DevDojoWebMvcConfigurer.java
@@ -0,0 +1,19 @@
+package academy.devdojo.springboot2.configurer;
+
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class DevDojoWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver pageHandler = new PageableHandlerMethodArgumentResolver();
+        pageHandler.setFallbackPageable(PageRequest.of(0, 5));
+        resolvers.add(pageHandler);
+    }
+}
