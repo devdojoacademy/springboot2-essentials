97756399c0b7bbf540815779e58d47a2c99117df
williamsuane <william.suane@devdojo.academy>
2020-04-21 19:44:27 +0200

    SpringBoot 2 Essentials 37 - Spring Security pt 06 - Adding Basic Authorization Integration Tests - TestRestTemplate

--- src/test/java/academy/devdojo/springboot2/integration/AnimeControllerIT.java
+++ src/test/java/academy/devdojo/springboot2/integration/AnimeControllerIT.java
@@ -13,11 +13,16 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -33,11 +38,38 @@ import org.springframework.http.ResponseEntity;
 class AnimeControllerIT {
 
     @Autowired
-    private TestRestTemplate testRestTemplate;
-    @LocalServerPort
-    private int port;
+    @Qualifier(value = "testRestTemplateRoleUser")
+    private TestRestTemplate testRestTemplateRoleUser;
+
+    @Autowired
+    @Qualifier(value = "testRestTemplateRoleAdmin")
+    private TestRestTemplate testRestTemplateRoleAdmin;
+
     @MockBean
     private AnimeRepository animeRepositoryMock;
+
+    @Lazy
+    @TestConfiguration
+    static class Config {
+
+        @Bean(name = "testRestTemplateRoleUser")
+        public TestRestTemplate testRestTemplateRoleUserCreator(@Value("${local.server.port}") int port) {
+            RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder()
+                .rootUri("http://localhost:" + port)
+                .basicAuthentication("devdojo", "academy");
+
+            return new TestRestTemplate(restTemplateBuilder);
+        }
+
+        @Bean(name = "testRestTemplateRoleAdmin")
+        public TestRestTemplate testRestTemplateRoleAdminCreator(@Value("${local.server.port}") int port) {
+            RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder()
+                .rootUri("http://localhost:" + port)
+                .basicAuthentication("william", "academy");
+
+            return new TestRestTemplate(restTemplateBuilder);
+        }
+    }
 
     @BeforeEach
     public void setUp() {
@@ -66,7 +98,7 @@ class AnimeControllerIT {
         String expectedName = AnimeCreator.createValidAnime().getName();
 
         //@formatter:off
-        Page<Anime> animePage = testRestTemplate.exchange("/animes", HttpMethod.GET,
+        Page<Anime> animePage = testRestTemplateRoleUser.exchange("/animes", HttpMethod.GET,
             null, new ParameterizedTypeReference<PageableResponse<Anime>>() {}).getBody();
         //@formatter:on
 
@@ -82,7 +114,7 @@ class AnimeControllerIT {
     public void findById_ReturnListOfAnimesInsidePageObject_WhenSuccessful() {
         Integer expectedId = AnimeCreator.createValidAnime().getId();
 
-        Anime anime = testRestTemplate.getForObject("/animes/1", Anime.class);
+        Anime anime = testRestTemplateRoleUser.getForObject("/animes/1", Anime.class);
 
         Assertions.assertThat(anime).isNotNull();
 
@@ -97,7 +129,7 @@ class AnimeControllerIT {
         String expectedName = AnimeCreator.createValidAnime().getName();
 
         //@formatter:off
-        List<Anime> animeList = testRestTemplate.exchange("/animes/find?name='tensei'",
+        List<Anime> animeList = testRestTemplateRoleUser.exchange("/animes/find?name='tensei'",
             HttpMethod.GET, null, new ParameterizedTypeReference<List<Anime>>() {
             }).getBody();
         //@formatter:on
@@ -116,7 +148,7 @@ class AnimeControllerIT {
 
         Anime animeToBeSaved = AnimeCreator.createAnimeToBeSaved();
 
-        Anime anime = testRestTemplate.exchange("/animes", HttpMethod.POST,
+        Anime anime = testRestTemplateRoleUser.exchange("/animes", HttpMethod.POST,
             createJsonHttpEntity(animeToBeSaved), Anime.class).getBody();
 
         Assertions.assertThat(anime).isNotNull();
@@ -130,7 +162,7 @@ class AnimeControllerIT {
     @DisplayName("delete removes the anime when successful")
     public void delete_RemovesAnime_WhenSuccessful() {
 
-        ResponseEntity<Void> responseEntity = testRestTemplate.exchange("/animes/1", HttpMethod.DELETE,
+        ResponseEntity<Void> responseEntity = testRestTemplateRoleAdmin.exchange("/animes/admin/1", HttpMethod.DELETE,
             null, Void.class);
 
         Assertions.assertThat(responseEntity).isNotNull();
@@ -141,11 +173,23 @@ class AnimeControllerIT {
     }
 
     @Test
+    @DisplayName("delete returns forbidden when user does not have the role admin")
+    public void delete_Returns403_WhenUserIsNotAdmin() {
+
+        ResponseEntity<Void> responseEntity = testRestTemplateRoleUser.exchange("/animes/admin/1", HttpMethod.DELETE,
+            null, Void.class);
+
+        Assertions.assertThat(responseEntity).isNotNull();
+
+        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+    }
+
+    @Test
     @DisplayName("update save updated anime when successful")
     public void update_SaveUpdatedAnime_WhenSuccessful() {
         Anime validAnime = AnimeCreator.createValidAnime();
 
-        ResponseEntity<Void> responseEntity = testRestTemplate.exchange("/animes", HttpMethod.PUT,
+        ResponseEntity<Void> responseEntity = testRestTemplateRoleUser.exchange("/animes", HttpMethod.PUT,
             createJsonHttpEntity(validAnime), Void.class);
 
         Assertions.assertThat(responseEntity).isNotNull();
